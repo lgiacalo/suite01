626a2394c2977b60878574ad22cf7fca487d0d0f
AlexLoWCD <alexandre.lorion@wildcodeschool.fr>
2018-04-04 11:00:36 +0200

    Ajout exo 2

---
+++ objects/exo2.js
@@ -0,0 +1,25 @@
+/*
+  Ecrire une classe "Voyage"
+  qui aura les propriétés suivantes :
+  - destination : la ville de destination du Voyage
+  - vehicule : le moyen de transport utilisé
+  - nbTravelers : le nombre de personnes présentes
+  - nbKilometers : le nombre de kilomètres parcourus
+
+  Cette classe pourra faire les actions suivantes :
+  - displayTravel : retourne la chaîne de caractère suivante :
+  "<nbTravelers> voyageurs participent au voyage
+  à destination de <destination> à bord de : <vehicule>"
+  - travel : ajoute un nombre de kilomètre passé en paramètre au nombre
+  total de kilomètres parcourus
+
+  Prototype:
+      class Voyage(str, str, int, int);
+*/
+
+//  écrire votre code sous ce commentaire
+
+/* DO NOT TOUCH */
+module.exports = {
+  Voyage: Voyage
+}
--- tests/objects.spec.js
+++ tests/objects.spec.js
@@ -1,4 +1,4 @@
-describe("Objects", () => {
+xdescribe("Objects", () => {
 
   /* Import exercices */
   // const exo1 = require('../objects/exo1');
@@ -24,11 +24,40 @@ describe("Objects", () => {
 
   /* Test Exercice 2 */
   xdescribe('Objects - Exo 2', () => {
-    it('should return [2, 6, 8]', () => {
 
+    const v1 = new exo2.Voyage("Paris", "Moto", 5, 0);
+
+    it('should have attribute "destination"', () => {
+      expect(v1.hasOwnProperty('destination')).toBeTruthy();
     });
 
-    it('should return []', () => {
+    it('should have attribute "vehicule"', () => {
+      expect(v1.hasOwnProperty('vehicule')).toBeTruthy();
+    });
+
+    it('should have attribute "nbTravelers"', () => {
+      expect(v1.hasOwnProperty('nbTravelers')).toBeTruthy();
+    });
+
+    it('should have attribute "nbKilometers"', () => {
+      expect(v1.hasOwnProperty('nbKilometers')).toBeTruthy();
+    });
+
+    it('should have method "displayTravel"', () => {
+      expect(v1.displayTravel).toBeDefined();
+    });
+
+    it('should have method "travel"', () => {
+      expect(v1.travel).toBeDefined();
+    });
+
+    it('should display the travel', () => {
+      expect(v1.displayTravel()).toEqual("5 voyageurs participent au voyage à destination de Paris à bord de : Moto");
+    });
+
+    it('should add kilometers', () => {
+      v1.travel(20);
+      expect(v1.nbKilometers).toEqual(20);
     });
   });
 
